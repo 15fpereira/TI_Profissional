21047581925e25cde8df5cd57d36eec8ea408264
Francisco Dias A. Pereira <franciscod.pereira@outlook.com>
2020-09-20 19:11:47 -0400

    Configuração do PAM integrado com Winbind e SAMBA-4
    # Configuração do PAM (Pluggable Authentication Modules for Linux) integrado com Winbind e SAMBA-4

--- script-25.sh
+++ script-25.sh
@@ -50,82 +50,64 @@ then
 					 clear
 					 
 					 echo -e "Atualizando as configuarações do PAM, pressione <Enter>."
-					 echo -e "Habilitar o recurso de: Create home directory on login"
-					 read
+					 read "Habilitar o recurso de: Create home directory on login"
 					 sleep 2
 					 clear
-					 
 					 #Executando comando pam-auth-update
 					 pam-auth-update
-					 
-					 echo -e "Atualização feita com sucesso!!!, pressione <Enter> para continuar com o script."
-					 read
+					 read "Atualização feita com sucesso!!!, pressione <Enter> para continuar com o script."
 					 sleep 2
 					 clear
-					 
-					 echo -e "Atualizando os arquivos de configuração do PAM, pressione <Enter> para continuar..."
-					 read
+					 read "Atualizando os arquivos de configuração do PAM, pressione <Enter> para continuar..."
 					 sleep 2
 					 echo
-					 
 					 echo -e "Fazendo o backup do arquivo de configuração COMMON-PASSWORD, aguarde..."
 					 #Backup deve ser feito em outro local o arquivo de configuração
 					 cp -v /etc/pam.d/common-password /var/log/common-password.old >> $LOG
 					 echo -e "Arquivo backupeado com sucesso!!!, continuando o script..."
 					 sleep 2
 					 echo
-					 
 					 echo -e "Atualizando o arquivo de configuração COMMON-PASSWORD, aguarde..."
 					 #Atualizando o arquivo de configuração
 					 cp -v conf/common-password /etc/pam.d/common-password >> $LOG
 					 echo -e "Arquivo e atualizado com sucesso!!!, continuando o script..."
 					 sleep 2
 					 echo
-					 
-					 echo -e "Editando o arquivo de configuração COMMON-PASSWORD, pressione <Enter> para continuar..."
-					 read
+					 read "Editando o arquivo de configuração COMMON-PASSWORD, pressione <Enter> para continuar..."
 					 sleep 2
 					 #Editando o arquivo de configuração
 					 vim /etc/pam.d/common-password
-					 echo -e "Arquivo editado com sucesso!!!, pressione <Enter> para continuando o script..."
-					 read
+					 read "Arquivo editado com sucesso!!!, pressione <Enter> para continuando o script..."
 					 sleep 2
 					 clear
-					 
 					 echo -e "Reinicializando o serviço do Winbind, aguarde..."
 					 #Reinicializando o serviço
 					 sudo service winbind restart >> $LOG
 					 echo -e "Serviços reinicializado com sucesso!!!, contiuando o script..."
 					 sleep 2
 					 echo
-					 
 					 echo -e "Adicionando o usuário $ADMIN ao grupo do SUDO, aguarde..."
 					 #Alterando o grupo do usuário administrator
 					 usermod -aG sudo $DOMAIN >> $LOG
 					 echo -e "Grupo adicionado com sucesso!!!, continuado com o script..."
 					 echo
-					 
 					 echo -e "Verificando as informação do Winbind do usuário $ADMIN, aguarde..."
 					 echo
 					 #Verificando as configurações de integração do Winbindo do usuário administrator
 					 wbinfo -i $ADMIN
 					 echo
-					 echo -e "Informações do Winbind obtidas com sucesso!!!, pressione <Enter> para continuar..."
-					 read
+					 read "Informações do Winbind obtidas com sucesso!!!, pressione <Enter> para continuar..."
 					 sleep 2
 					 clear
 					 echo
-					 
 					 echo -e "Criando o diretório base do usuário $ADMIN"
 					 echo -e "Pressione <Enter> para continuar"
-					 echo -e "Para sair do login do usuário $ADMIN, digite exit <Enter>"
-					 read
+					 read "Para sair do login do usuário $ADMIN, digite exit <Enter>"
 					 sleep 2
 					 #Criando a estruura de diretório e acesso a pasta pessoal
 					 su - $ADMIN >> $LOG
 					 echo -e "Diretório criado com sucesso!!!, continuando o script..."
 					 echo
-					 
 					 echo -e "Fim do Script-25.sh em: `date`" >> $LOG
 					 echo -e "    Finalização da Configuração da Integração do PAM com Winbind e SAMBA-4"
 					 echo -e "================================================================================="
@@ -136,20 +118,16 @@ then
 						RESULTADO=`expr 10800 + $SOMA`
 						TEMPO=`date -d @$RESULTADO +%H:%M:%S`
 					 echo -e "Tempo gasto para execução do script-25.sh: $TEMPO"
-					 echo -e "Pressione <Enter> para concluir o processo."
-					 read
+					 read "Pressione <Enter> para concluir o processo."
 					 else
 						 echo -e "Versão do Kernel: $KERNEL não homologada para esse script, versão: >= 4.4 "
-						 echo -e "Pressione <Enter> para finalizar o script"
-					 read
+						 read "Pressione <Enter> para finalizar o script"
 			fi
 		else
 			 echo -e "Distribuição GNU/Linux: `lsb_release -is` não homologada para esse script, versão: $UBUNTU"
-			 echo -e "Pressione <Enter> para finalizar o script"
-			read
+			 read "Pressione <Enter> para finalizar o script"
 		fi
 else
 	 echo -e "Usuário não é ROOT, execute o comando com a opção: sudo -i <Enter> depois digite a senha do usuário `whoami`"
-	 echo -e "Pressione <Enter> para finalizar o script"
-	read
+	 read "Pressione <Enter> para finalizar o script"
 fi
